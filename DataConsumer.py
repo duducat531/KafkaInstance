7770ddf143c348b2a9ef6ee8d6f4c39bebde13e8
duducat531 <wangyining531@gmail.com>
2018-10-16 14:53:26 +0800

    remove sample

--- DataConsumer.py
+++ DataConsumer.py
@@ -13,9 +13,3 @@ class DataConsumer:
         for data in consumer:
             # save data to mysql
             self.dbInstance.SaveToDB(data)
-
-if __name__ == '__main__':
-    logDataConsumer = DataConsumer(topicName='ebay_metric', serverAddr='localhost:9092',
-                                   groupId='test_log_data_consumer',
-                                   dbInstance=DBInstance("localhost", "wyn", "123456", "ebay_log"))
-    logDataConsumer.ReceiveAndSaveToDB()
