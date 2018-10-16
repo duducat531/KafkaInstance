1be67d6a5510376c38ba8085b224c16dc2209c2c
duducat531 <wangyining531@gmail.com>
2018-10-16 14:53:06 +0800

    remove sample code

--- DataProducer.py
+++ DataProducer.py
@@ -10,12 +10,3 @@ class DataProducer:
 
     def PublishData(self, data):
         self.producer.send(self.topicName, data)
-
-if __name__ == '__main__':
-    logDataConsumer = DataProducer(topicName='ebay_log', serverAddr='localhost:9092', groupId='test_log_data_producer')
-    logData = {'corr_id': 'ejsQ5ufDFCmyHG6v',
-               'url_api': 'sample/login',
-               'node_id': 'NSflDxKmr2AR',
-               'ri': 'OgXI76Q5z9++',
-               'parent_ri': 'NSflDxKmr2AAR'}
-    logDataConsumer.PublishData(logData)
